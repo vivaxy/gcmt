5b286732387665c066f2c8ab7e0fc6076e260646
vivaxy <xyxuye2007@126.com>
2015-08-23 14:35:52 +0800

    test

--- gcmt.plugin.zsh
+++ gcmt.plugin.zsh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/usr/bin/env sh
 # vivaxy@20150823
 gcmt(){
     log(){
--- update.sh
+++ update.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/usr/bin/env sh
 
 cd ~/.oh-my-zsh/custom/plugins/gcmt
 git pull
